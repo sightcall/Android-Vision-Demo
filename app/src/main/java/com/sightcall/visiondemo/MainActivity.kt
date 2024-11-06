76c062a1c7501d9b7b0b4647bee5a34a794c9586
RiadhMezzi <riadh.mezzi@sightcall.com>
2024-11-06 16:03:31 +0100

    add input field for flow url

--- app/src/main/java/com/sightcall/visiondemo/MainActivity.kt
+++ app/src/main/java/com/sightcall/visiondemo/MainActivity.kt
@@ -2,16 +2,34 @@ package com.sightcall.visiondemo
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sightcall.core.experiences.SightCall
 import com.sightcall.visiondemo.ui.theme.VisionDemoTheme
 
@@ -21,34 +39,66 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VisionDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Sigthcall vision sdk demo",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainActivityContent()
             }
         }
-
-        val uri = Uri.parse("YOUR_URL_HERE")
-        SightCall.start(uri)
-
 
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainActivityContent() {
+    val context = LocalContext.current
+    val clipboardManager = LocalClipboardManager.current
+    var textState by remember { mutableStateOf(TextFieldValue()) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text("Enter URL") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            clipboardManager.getText()?.let { clipboardText ->
+                textState = TextFieldValue(clipboardText.text)
+            } ?: run {
+                Toast.makeText(context, "Clipboard is empty", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            Text("Paste from Clipboard")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                val enteredUrl = textState.text
+                val uri = Uri.parse(enteredUrl)
+                try {
+                    SightCall.start(uri)
+                } catch (e: Exception) {
+                    Toast.makeText(context, "Invalid URL", Toast.LENGTH_SHORT).show()
+                }
+            },
+            colors = ButtonDefaults.buttonColors(Color.Magenta)
+        ) {
+            Text("Launch Flow", color = Color.White)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     VisionDemoTheme {
-        Greeting("Sightcall")
+        MainActivityContent()
     }
 }
+
